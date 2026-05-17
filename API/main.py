2c1d0de674b5f57a037751d044803f8c2cac4c6d
Natalia Garcia Rincon <n.garciar23@uniandes.edu.co>
2026-05-17 03:12:53 -0500

    cambios main

--- API/main.py
+++ API/main.py
@@ -18,7 +18,12 @@ app.add_middleware(
 # TODO: conectarse al cluster Admonsis  
 # client = MongoClient("mongodb://<usuario>:<contraseña>@157.253.236.88:8087")
 
-client = MongoClient(os.environ["MONGO_URI"])
+client = MongoClient(
+    os.environ["MONGO_URI"],
+    tls=True,
+    tlsAllowInvalidCertificates=True
+)
+
 db = client["parranderos_nosql"]
 
 
